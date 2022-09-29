717efc4a95f1e692bbb8db27ce47b1f04281a4e1
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2022-09-29 19:17:07 -0300

    Create LIMIT.sql

---
+++ Scripts/LIMIT.sql
@@ -0,0 +1,5 @@
+SELECT * FROM employees ORDER BY birth_date ASC LIMIT 5;
+/*Limita somente à 5 resultados*/
+
+SELECT * FROM employees ORDER BY birth_date DESC LIMIT 50; 
+/*Limita a 50 resultados, por ordem decrescente*/
